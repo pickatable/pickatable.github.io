2ecea731034b22f5e7d2aede7389577ca1cedf8d
Alessandro Aime <alessandro.aime@hotmail.com>
2024-11-13 12:15:39 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+/* document.addEventListener('DOMContentLoaded', function () {
+    var background = document.querySelector('.background-image');
+
+    window.addEventListener('scroll', function () {
+        var yPos = -window.scrollY / 2;
+        background.style.backgroundPosition = 'center ' + yPos + 'px';
+    });
+});
+
+ */
