ee9e3fcab700c9677d777ed89f0153750efb76a9
Alessandro Aime <alessandro.aime@hotmail.com>
2024-11-18 15:11:42 +0200

    Updated to absolute paths

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,6 @@
 const loadFooter = async () => {
     try {
-        const response = await fetch('components/footer.html');
+        const response = await fetch('/components/footer.html');
         if (!response.ok) throw new Error('Failed to load footer');
         const footer = await response.text();
         document.getElementById('footer').innerHTML = footer;
